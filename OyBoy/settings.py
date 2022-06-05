50d0c951ab1a92da2014f76e70f4b72a8039232e
illia-kravets <illia.kravets@datawiz.io>
2022-06-05 15:37:10 +0300

    database conf

--- OyBoy/settings.py
+++ OyBoy/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'OyBoy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'oyboy',
+        'USER': 'oyboy',
+        'PASSWORD': 'DW1515Asd!324DW',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
