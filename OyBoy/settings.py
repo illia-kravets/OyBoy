c25cc59d074c827eec1fe9902075c2e88c3af3f5
illia-kravets <illia.kravets@datawiz.io>
2022-06-22 21:39:23 +0300

    changed token lifetime

--- OyBoy/settings.py
+++ OyBoy/settings.py
@@ -165,7 +165,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
 }
